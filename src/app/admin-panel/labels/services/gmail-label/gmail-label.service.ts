d125fa9603ba0d810b4994259714bf2f0992479d
Benjamin Blumenfeld-Jones <BensThoughts@GMail.com>
2018-12-29 00:41:59 -0700

    removed throwError, catchError (rxjs), and HttpError from gmail-label.service...these are now all handled by effects and the appwide http error interceptor

--- src/app/admin-panel/labels/services/gmail-label/gmail-label.service.ts
+++ src/app/admin-panel/labels/services/gmail-label/gmail-label.service.ts
@@ -1,7 +1,7 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {Observable, throwError } from 'rxjs';
-import { catchError, retry, map } from 'rxjs/operators';
+import { Injectable} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { retry, map } from 'rxjs/operators';
 
 // import { GmailLabel } from './models/gmail-label.model';
 
