050fac25c8714dc4e4ae6a7a7d6822381a56b99a
Benjamin Blumenfeld-Jones <BensThoughts@GMail.com>
2019-01-15 02:27:16 -0700

    colors working on toolbar, and in general..now to implement sitewide

--- src/app/core/state/meta-reducers/local-storage-sync.reducer.ts
+++ src/app/core/state/meta-reducers/local-storage-sync.reducer.ts
@@ -5,7 +5,7 @@ import { AppState } from '../core.state';
 export function localStorageSyncReducer(reducer: ActionReducer<AppState>): ActionReducer<AppState> {
   return localStorageSync(
     {
-      keys: ['user', 'auth', 'gmailLabel', 'suggested'],
+      keys: ['user', 'auth', 'gmailLabel', 'suggested', 'settings'],
       rehydrate: true,
       removeOnUndefined: true,
       storageKeySerializer: (key) => 'go-app-' + key
