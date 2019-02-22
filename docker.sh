45f37bda48ee2140b7c82756b37a8c29d6839d82
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-02-21 17:41:11 -0700

    new docker.sh

--- docker.sh
+++ docker.sh
@@ -8,18 +8,19 @@ if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
     exit 1
 fi
 
-usage="$(basename "$0") [ -h | --help ] [ -b | --build ] [ -p | --push ] -- program to build and push docker container images"
+usage="$(basename "$0") [ -h | --help ] [-i | --info] [ -b | --build ] [ -p | --push ] -- program to build and push docker container images"
 echo "$usage"
 
 help="
 where:
 	-h, --help:  help
+	-i, --info: get the current projects repo/name
 	-b, --build: build container
 	-p, --push:  push a new version (patch, minor, or major)"
 
 
-OPTIONS=hbp:v
-LONGOPTS=help,build,push:,verbose
+OPTIONS=hibp:
+LONGOPTS=help,info,build,push:,
 
 # -use ! and PIPESTATUS to get exit code with errexit set
 # -temporarily store output to be able to check for errors
@@ -34,7 +35,7 @@ fi
 # read getopt’s output this way to handle the quoting right:
 eval set -- "$PARSED"
 
-h=n b=n p=n v=n pushMode=-
+h=n b=n i=n pushMode=-
 # now enjoy the options in order and nicely split until we see --
 while true; do
     case "$1" in
@@ -42,6 +43,10 @@ while true; do
             h=y
             shift
             ;;
+		-i|--info)
+			i=y
+			shift
+			;;
         -b|--build)
             b=y
             shift
@@ -49,10 +54,6 @@ while true; do
         -p|--push)
             pushMode="$2"
             shift 2
-            ;;
-        -v|--verbose)
-            v=y
-            shift
             ;;
         --)
             shift
@@ -65,86 +66,67 @@ while true; do
     esac
 done
 
-# handle non-option arguments
-# if [[ $# -ne 1 ]]; then
-#    echo "$0: A push mode is required"
-#    exit 4
-# fi
-
 source ./deploy.env
 IMG_NAME=$DOCKER_REPO/$APP_NAME
-echo "verbose: $v, build: $b, push: $pushMode, help: $h"
 
 if [ "$h" = "y" ]; then
     printf "$help\n"
 fi
 
+if [ "$i" = "y" ]; then
+	printf "Repo/App: $IMG_NAME\n"
+fi
+
 if [ "$b" = "y" ]; then
-    printf "Building docker image: \n"
 	GIT_VER=$(git rev-parse @)
-	PREV_GIT_VER=$(git rev-parse @~)
 	PREV_ID=$(docker images -f reference=$IMG_NAME:latest --format "{{.ID}}")
-	PREV_GIT_TAG=$(docker images -f reference=$IMG_NAME:$PREV_GIT_VER --format "{{.Tag}}")
-	printf "Image: $IMG_NAME:$GIT_VER\n"
+	# Start by removing all previous versions of the image
+	if [ ! -z PREV_ID ]; then
+		docker image rm -f "$PREV_ID"
+	fi
+	# Build and tag the image with the git commit hash
+	printf "Building docker image: $IMG_NAME:$GIT_VER\n"
 	docker build -t $IMG_NAME:latest .
 	docker tag $IMG_NAME:latest $IMG_NAME:$GIT_VER
-	CUR_ID=$(docker images -f reference=$IMG_NAME:latest --format "{{.ID}}")
-	if [ "$PREV_ID" != "$CUR_ID" ]; then
-		docker image rm $PREV_ID
-	elif [ ! -z "$PREV_GIT_TAG" ]; then
-		docker image rm "$IMG_NAME:$PREV_GIT_TAG"
-	fi
 fi
 
 if [ "$pushMode" != "-" ]; then
     case "$pushMode" in
         patch)
-             PREV_SEM_VER="v$(jq -rM '.version' package.json)"
 			 NEW_SEM_VER=$(npm version patch)
-			 printf "Previous sem version: $PREV_SEM_VER\n"
-			 printf "New sem version: $NEW_SEM_VER\n"
-			 NEW_GIT_VER=$(git rev-parse @)
-			 PREV_GIT_VER=$(git rev-parse @~)
-			 docker tag $IMG_NAME:latest $IMG_NAME:$NEW_GIT_VER
-			 docker image rm $IMG_NAME:$PREV_GIT_VER
-			 docker push $IMG_NAME:$NEW_GIT_VER
-			 docker push $IMG_NAME:latest
-			 docker tag $IMG_NAME:$NEW_GIT_VER $IMG_NAME:$NEW_SEM_VER
-			 docker push $IMG_NAME:$NEW_SEM_VER
-			 docker image rm $IMG_NAME:$NEW_SEM_VER
             ;;
         minor)
-             PREV_SEM_VER="v$(jq -rM '.version' package.json)"
 			 NEW_SEM_VER=$(npm version minor)
-			 printf "Previous sem version: $PREV_SEM_VER\n"
-			 printf "New sem version: $NEW_SEM_VER\n"
-			 NEW_GIT_VER=$(git rev-parse @)
-			 PREV_GIT_VER=$(git rev-parse @~)
-			 docker tag $IMG_NAME:latest $IMG_NAME:$NEW_GIT_VER
-			 docker image rm $IMG_NAME:$PREV_GIT_VER
-			 docker push $IMG_NAME:$NEW_GIT_VER
-			 docker push $IMG_NAME:latest
-			 docker tag $IMG_NAME:$NEW_GIT_VER $IMG_NAME:$NEW_SEM_VER
-			 docker push $IMG_NAME:$NEW_SEM_VER
-			 docker image rm $IMG_NAME:$NEW_SEM_VER
             ;;
         major)
-             PREV_SEM_VER="v$(jq -rM '.version' package.json)"
 			 NEW_SEM_VER=$(npm version major)
-			 printf "Previous sem version: $PREV_SEM_VER\n"
-			 printf "New sem version: $NEW_SEM_VER\n"
-			 NEW_GIT_VER=$(git rev-parse @)
-			 PREV_GIT_VER=$(git rev-parse @~)
-			 docker tag $IMG_NAME:latest $IMG_NAME:$NEW_GIT_VER
-			 docker image rm $IMG_NAME:$PREV_GIT_VER
-			 docker push $IMG_NAME:$NEW_GIT_VER
-			 docker push $IMG_NAME:latest
-			 docker tag $IMG_NAME:$NEW_GIT_VER $IMG_NAME:$NEW_SEM_VER
-			 docker push $IMG_NAME:$NEW_SEM_VER
-			 docker image rm $IMG_NAME:$NEW_SEM_VER
-            echo major
             ;;
         *)
-            echo push must be one of patch, minor, or major
+            printf "push must be one of patch, minor, or major\n"
+			exit 1
     esac
+	
+	printf "New sem version: $NEW_SEM_VER\n"
+	
+	# Tag the new commit version and remove the old tag
+	# (npm version bump creates a new commit)
+	NEW_GIT_VER=$(git rev-parse @)
+	PREV_GIT_VER=$(git rev-parse @~)
+	docker tag $IMG_NAME:latest $IMG_NAME:$NEW_GIT_VER
+	docker image rm $IMG_NAME:$PREV_GIT_VER
+	
+	# Push the newest commit, and latest tag
+	printf "Pushing git version to docker repo: $NEW_GIT_VER"
+	docker push $IMG_NAME:$NEW_GIT_VER
+	printf "Pushing tag latest to docker repo:"
+	docker push $IMG_NAME:latest
+
+	# Create the semantic version tag, push it, then remove it
+	# from the local system.
+	printf "Pushing tag $NEW_SEM_VER to docker repo:"
+	docker tag $IMG_NAME:$NEW_GIT_VER $IMG_NAME:$NEW_SEM_VER
+	docker push $IMG_NAME:$NEW_SEM_VER
+	docker image rm $IMG_NAME:$NEW_SEM_VER
 fi
+
+exit 0
