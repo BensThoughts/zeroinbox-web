8f4b47a19228f8a6e7a240f0164325b8d52c651d
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-02-23 19:18:41 -0700

    updated prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,24 +1,22 @@
 const packageJson = require('../../package.json');
 
 export const environment = {
-  appName: 'Gmail Starter',
+  appName: packageJson.name,
+  appVersion: packageJson.version,
   envName: 'PROD',
   apiHost: 'zeroinbox.app',
   apiPort: 3000,
   production: true,
   test: false,
-  i18nPrefix: '/gmail-organizer',
-  versions: {
-    app: packageJson.version,
+  i18nPrefix: '',
+  packageVersions: {
     angular: packageJson.dependencies['@angular/core'],
     ngrx: packageJson.dependencies['@ngrx/store'],
     material: packageJson.dependencies['@angular/material'],
     bootstrap: packageJson.dependencies.bootstrap,
     rxjs: packageJson.dependencies.rxjs,
-    ngxtranslate: packageJson.dependencies['@ngx-translate/core'],
-    fontAwesome: packageJson.dependencies['@fortawesome/fontawesome-free-webfonts'],
+    fontAwesome: packageJson.dependencies['@fortawesome/fontawesome-free'],
     angularCli: packageJson.devDependencies['@angular/cli'],
     typescript: packageJson.devDependencies['typescript'],
-    cypress: packageJson.devDependencies['cypress']
   }
 };
