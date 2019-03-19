c6baa693f2a270087dd677a3e3dfb10f0533ba7e
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-03-19 00:38:34 -0700

    added testData

--- src/app/admin-panel/subscriptions/components/subscriptions.component.ts
+++ src/app/admin-panel/subscriptions/components/subscriptions.component.ts
@@ -2,25 +2,27 @@ import { Component, OnInit, ViewChild, ChangeDetectionStrategy } from '@angular/
 import { Store, select } from '@ngrx/store';
 import { AppState } from '@app/core';
 import { selectSubscriptionSenders } from '../state/subscriptions.selectors';
-import { MatSort, MatPaginator, PageEvent } from '@angular/material';
-import { Observable } from 'rxjs';
+import { MatSort, MatPaginator, PageEvent, Sort } from '@angular/material';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { fromMatPaginator, paginateRows } from '../state/datasource-utils';
+import { fromMatPaginator, paginateRows, fromMatSort, sortRows } from '../state/datasource-utils';
 import { ISender } from '@app/core/state/senders/model/senders.model';
 
 @Component({
   selector: 'app-subscriptions-component',
   templateUrl: './subscriptions.component.html',
   styleUrls: ['./subscriptions.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SubscriptionsComponent implements OnInit {
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   subscriptions$: Observable<ISender[]>;
+  // subscriptions$: Subscription;
   totalRows$: Observable<number>;
-  displayedRows$: Observable<ISender[]>;
+  displayedRows$: Observable<TestData[]>;
 
   pageSize = 10;
   pageSizeOptions: number[] = [5, 10, 25, 100];
@@ -29,9 +31,41 @@ export class SubscriptionsComponent implements OnInit {
 
   ngOnInit() {
     const pageEvents$: Observable<PageEvent> = fromMatPaginator(this.paginator);
-    this.subscriptions$ = this.store.pipe(select(selectSubscriptionSenders));
-    this.displayedRows$ = this.subscriptions$.pipe(paginateRows(pageEvents$)); 
-    this.totalRows$ = this.subscriptions$.pipe(map(rows => rows.length));
+    const sortEvents$: Observable<Sort> = fromMatSort(this.sort);
+    const rows$ = of(testData);
+
+    // this.subscriptions$ = this.store.pipe(select(selectSubscriptionSenders));
+    // this.displayedRows$ = this.subscriptions$.pipe(
+    //  sortRows(sortEvents$),
+    //  paginateRows(pageEvents$)
+    // );
+    this.displayedRows$ = rows$.pipe(
+      sortRows(sortEvents$),
+      paginateRows(pageEvents$)
+    )
+    this.totalRows$ = rows$.pipe(
+      map(rows => rows.length)
+    ) 
+    // this.totalRows$ = this.subscriptions$.pipe(map(rows => rows.length));
   }
 
 }
+
+export interface TestData {
+  fromName: string;
+  count: number;
+  fromAddress: string;
+}
+
+export const testData: TestData[] = [
+  {
+    fromName: 'test1',
+    fromAddress: 'test1',
+    count: 10
+  },
+  {
+    fromName: 'test2',
+    fromAddress: 'test2',
+    count: 5
+  }
+]
