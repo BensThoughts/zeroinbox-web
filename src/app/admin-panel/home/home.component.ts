fc2c86306e3cf9bf68670c6fafb7aae22966a9d5
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-03-18 22:52:58 -0700

    rough attempt at caching selectors in home component

--- src/app/admin-panel/home/home.component.ts
+++ src/app/admin-panel/home/home.component.ts
@@ -11,8 +11,10 @@ import {
 } from '@app/core';
 
 import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { BasicProfile, EmailProfile } from '@app/core/state/user/user.model';
+import { selectSendersFromSuggestionIds, selectSendersNotLabeledBySize } from '../suggestions/state/suggestions.selectors';
+import { selectSubscriptionSenders } from '../subscriptions/state/subscriptions.selectors';
 
 @Component({
   selector: 'app-home',
@@ -30,6 +32,10 @@ export class HomeComponent implements OnInit {
   unique_senders$: Observable<number>;
   sendersLoaded$;
 
+  labelByNameCache$: Subscription;
+  labelBySizeCache$: Subscription;
+  subscriptionSendersCache$: Subscription;
+
   constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
@@ -38,6 +44,24 @@ export class HomeComponent implements OnInit {
     
     this.unique_senders$ = this.store.pipe(select(selectUniqueSenders));
     this.sendersLoaded$ = this.store.pipe(select(selectSendersLoaded));
+
+    this.labelByNameCache$ = this.store.pipe(
+      select(selectSendersFromSuggestionIds)
+      ).subscribe();
+
+    this.labelBySizeCache$ = this.store.pipe(
+      select(selectSendersNotLabeledBySize)
+    ).subscribe();
+
+    this.subscriptionSendersCache$ = this.store.pipe(
+      select(selectSubscriptionSenders)
+    ).subscribe();
+  }
+
+  ngOnDestroy() {
+    this.labelByNameCache$.unsubscribe();
+    this.labelBySizeCache$.unsubscribe();
+    this.subscriptionSendersCache$.unsubscribe();
   }
 
 }
