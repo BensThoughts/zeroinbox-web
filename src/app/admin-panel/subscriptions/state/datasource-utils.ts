ce5a7bd9524e417e265d7ff48d1a4df2be917efd
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-03-19 00:38:06 -0700

    added sort

--- src/app/admin-panel/subscriptions/state/datasource-utils.ts
+++ src/app/admin-panel/subscriptions/state/datasource-utils.ts
@@ -1,18 +1,114 @@
 import { DataSource } from '@angular/cdk/table';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { CollectionViewer } from '@angular/cdk/collections';
-import { MatPaginator, PageEvent } from '@angular/material';
+import { MatPaginator, PageEvent, MatSort, Sort } from '@angular/material';
 import { concat } from 'rxjs/observable/concat';
 import { defer } from 'rxjs/observable/defer';
 import { of } from 'rxjs/observable/of';
 import { combineLatest } from 'rxjs/observable/combineLatest';
+import { Store, MemoizedSelector, select } from '@ngrx/store';
+import { AppState } from '@app/core';
+import { tap, map } from 'rxjs/operators';
 
 export class SimpleDataSource<T> extends DataSource<T> {
-    constructor(private rows$: Observable<T[]>) { super(); }
-    connect(collectionViewer: CollectionViewer): Observable<T[]> {
-        return this.rows$;
+    private dataSubject = new BehaviorSubject<T[]>([]);
+    
+    constructor(private store: Store<AppState>, private selector: MemoizedSelector<AppState, T[]>) { 
+        super(); 
     }
-    disconnect(collectionViewer: CollectionViewer): void {}
+
+    loadData() {
+        this.store.pipe(
+            select(this.selector),
+            tap((array) => {
+                this.dataSubject.next(array);
+            })
+        ).subscribe();
+    }
+
+    connect(collectionViewer: CollectionViewer): Observable<T[]> {
+        return this.dataSubject.asObservable();
+    }
+
+    disconnect(collectionViewer: CollectionViewer): void {
+        this.dataSubject.complete();
+    }
+}
+
+function defaultSort(a: any, b: any): number {
+    //treat null === undefined for sorting
+    a = a === undefined ? null : a;
+    b = b === undefined ? null : b;
+  
+    if (a === b) { return 0; }
+    if (a === null) { return -1; }
+    if (b === null) { return 1; }
+  
+    //from this point on a & b can not be null or undefined.
+  
+    if (a > b) {
+      return 1;
+    } else if (a < b) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+
+type SortFn<U> = (a: U, b: U) => number;
+interface PropertySortFns<U> {
+  [prop: string]: SortFn<U>;
+}
+
+/** RxJS operator to map a material Sort object to a sort function */
+function toSortFn<U>(sortFns: PropertySortFns<U> = {}, useDefault = true): (sort$: Observable<Sort>) => Observable<undefined | SortFn<U>> {
+    return (sort$) => sort$.pipe(
+      map(sort => {
+        if (!sort.active || sort.direction === '') { return undefined; }
+  
+        let sortFn = sortFns[sort.active];
+        if (!sortFn) {
+          if (!useDefault) {
+            throw new Error(`Unknown sort property [${sort.active}]`);
+          }
+  
+          //By default assume sort.active is a property name, and sort using the default sort
+          //  uses < and >.
+          sortFn = (a: U, b: U) => defaultSort((<any>a)[sort.active], (<any>b)[sort.active]);
+        }
+  
+        return sort.direction === 'asc' ? sortFn : (a: U, b: U) => sortFn(b, a);
+      })
+    );
+  }
+
+  /** Creates an Observable stream of Sort objects from a MatSort component */
+export function fromMatSort(sort: MatSort): Observable<Sort> {
+    return concat(
+      defer(() => of({
+        active: sort.active,
+        direction: sort.direction
+      })),
+      sort.sortChange.asObservable()
+    );
+  }
+
+  /** RxJs operator to sort an array based on an Observable of material Sort events **/
+export function sortRows<U>(
+    sort$: Observable<Sort>,
+    sortFns: PropertySortFns<U> = {},
+    useDefault = true
+  ): (obs$: Observable<U[]>) => Observable<U[]> {
+    return (rows$: Observable<U[]>) => combineLatest(
+      rows$,
+      sort$.pipe(toSortFn(sortFns, useDefault)),
+      (rows, sortFn) => {
+        if (!sortFn) { return rows; }
+  
+        const copy = rows.slice();
+        return copy.sort(sortFn);
+      }
+    );
 }
 
 /** Creates an Observable stream of PageEvent objects from a MatPaginator component */
