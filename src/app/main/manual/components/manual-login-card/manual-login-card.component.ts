5fa77c9eb69e7c09c54d201de58d86ad84300fb8
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-06-13 15:54:13 -0700

    ManualLoginCardComponent created

---
+++ src/app/main/manual/components/manual-login-card/manual-login-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-manual-login-card',
+  templateUrl: './manual-login-card.component.html',
+  styleUrls: ['./manual-login-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class ManualLoginCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+  }
+
+}
