6c76b5a368518e3ada6320619a334a0e8513aed5
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-07-31 23:19:45 -0700

    downsized tech avatars

--- src/app/main/story/tech.ts
+++ src/app/main/story/tech.ts
@@ -8,131 +8,131 @@ export const tech_stack: TechStackItem[] = [
   {
     name: 'Angular',
     url: 'https://angular.io',
-    imageSrc: 'assets/avatars/angular.png'
+    imageSrc: 'assets/avatars/angular-down.png'
   },
   {
     name: 'bash',
     url: 'https://www.gnu.org/software/bash/',
-    imageSrc: 'assets/avatars/bash.png'
+    imageSrc: 'assets/avatars/bash-down.png'
   },
   {
     name: 'Docker',
     url: 'https://www.docker.com/',
-    imageSrc: 'assets/avatars/docker.png'
+    imageSrc: 'assets/avatars/docker-down.png'
   },
   {
     name: 'eslint',
     url: 'https://eslint.org/',
-    imageSrc: 'assets/avatars/eslint.png'
+    imageSrc: 'assets/avatars/eslint-down.png'
   },
   {
     name: 'Express.js',
     url: 'https://expressjs.com/',
-    imageSrc: 'assets/avatars/expressjs.png'
+    imageSrc: 'assets/avatars/expressjs-down.png'
   },
   {
     name: 'Firebase',
     url: 'https://firebase.google.com/',
-    imageSrc: 'assets/avatars/firebase.png'
+    imageSrc: 'assets/avatars/firebase-down.png'
   },
   {
     name: 'fontawesome',
     url: 'https://fontawesome.com/',
-    imageSrc: 'assets/avatars/fontawesome.png'
+    imageSrc: 'assets/avatars/fontawesome-down.png'
   },
   {
     name: 'git',
     url: 'https://git-scm.com/',
-    imageSrc: 'assets/avatars/git.png'
+    imageSrc: 'assets/avatars/git-down.png'
   },
   {
     name: 'Google Analytics',
     url: 'https://analytics.google.com/',
-    imageSrc: 'assets/avatars/googleanalytics.png'
+    imageSrc: 'assets/avatars/googleanalytics-down.png'
   },
   {
     name: 'Google Cloud',
     url: 'https://cloud.google.com/',
-    imageSrc: 'assets/avatars/googlecloud.png'
+    imageSrc: 'assets/avatars/googlecloud-down.png'
   },
   {
     name: 'Kubernetes',
     url: 'https://cloud.google.com/kubernetes-engine',
-    imageSrc: 'assets/avatars/gke.png'
+    imageSrc: 'assets/avatars/gke-down.png'
   },
   {
     name: 'mochajs',
     url: 'https://mochajs.org/',
-    imageSrc: 'assets/avatars/mochajs.png'
+    imageSrc: 'assets/avatars/mochajs-down.png'
   },
   {
     name: 'MongoDB',
     url: 'https://www.mongodb.com/',
-    imageSrc: 'assets/avatars/mongodb.png'
+    imageSrc: 'assets/avatars/mongodb-down.png'
   },
   {
     name: 'ngx-translate',
     url: 'http://www.ngx-translate.com/',
-    imageSrc: 'assets/avatars/ngx-translate.png'
+    imageSrc: 'assets/avatars/ngx-translate-down.png'
   },
   {
     name: 'ngrx',
     url: 'https://ngrx.io/',
-    imageSrc: 'assets/avatars/ngrx.png'
+    imageSrc: 'assets/avatars/ngrx-down.png'
   },
   {
     name: 'node.js',
     url: 'https://nodejs.org/',
-    imageSrc: 'assets/avatars/node.png'
+    imageSrc: 'assets/avatars/node-down.png'
   },
   {
     name: 'npm',
     url: 'https://www.npmjs.com/',
-    imageSrc: 'assets/avatars/npm2.png'
+    imageSrc: 'assets/avatars/npm-down.png'
   },
   {
     name: 'Prettier',
     url: 'https://prettier.io/',
-    imageSrc: 'assets/avatars/prettier.png'
+    imageSrc: 'assets/avatars/prettier-down.png'
   },
   {
     name: 'RabbitMQ',
     url: 'https://www.rabbitmq.com/',
-    imageSrc: 'assets/avatars/rabbitmq.png'
+    imageSrc: 'assets/avatars/rabbitmq-down.png'
   },
   {
     name: 'redis',
     url: 'https://redis.io/',
-    imageSrc: 'assets/avatars/redis.png'
+    imageSrc: 'assets/avatars/redis-down.png'
   },
   {
     name: 'rxjs',
     url: 'https://rxjs.dev/',
-    imageSrc: 'assets/avatars/rxjs.png'
+    imageSrc: 'assets/avatars/rxjs-down.png'
   },
   {
     name: 'sinon.js',
     url: 'https://sinonjs.org/',
-    imageSrc: 'assets/avatars/sinon.png'
+    imageSrc: 'assets/avatars/sinon-down.png'
   },
   {
     name: 'tailwindcss',
     url: 'https://tailwindcss.com/',
-    imageSrc: 'assets/avatars/tailwindcss.png'
+    imageSrc: 'assets/avatars/tailwindcss-down.png'
   },
   {
     name: 'Typescript',
     url: 'https://www.typescriptlang.org/',
-    imageSrc: 'assets/avatars/typescript.png'
+    imageSrc: 'assets/avatars/typescript-down.png'
   },
   {
     name: 'Ubuntu',
     url: 'https://ubuntu.com/',
-    imageSrc: 'assets/avatars/ubuntu.png'
+    imageSrc: 'assets/avatars/ubuntu-down.png'
   },
   {
     name: 'VSCode',
     url: 'https://code.visualstudio.com/',
-    imageSrc: 'assets/avatars/vscode.png'
+    imageSrc: 'assets/avatars/vscode-down.png'
   }
 ];
