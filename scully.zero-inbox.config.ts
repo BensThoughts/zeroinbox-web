ca4e4eb67f1e5fbbde37d4611255a9b445febce1
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-08-15 22:49:31 -0700

    scully init

---
+++ scully.zero-inbox.config.ts
@@ -0,0 +1,7 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: './src',
+  projectName: 'zero-inbox',
+  outDir: './dist/static',
+  routes: {}
+};
