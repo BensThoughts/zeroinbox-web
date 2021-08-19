ca36ac04d4fd9aa249e7395c06894cbb73b7ab90
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-08-18 18:27:06 -0700

    removed deprectaed @Effect

--- src/app/core/state/user/user.effects.ts
+++ src/app/core/state/user/user.effects.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { filter, map, concatMap, catchError } from 'rxjs/operators';
 import { fromEvent, of } from 'rxjs';
-import { Effect, Actions, ofType } from '@ngrx/effects';
+import { Actions, ofType, createEffect } from '@ngrx/effects';
 import {
   UpdateUserStateAction,
   UserProfileRequestAction,
@@ -20,60 +20,64 @@ import { FirstRunStatusRequestAction } from '../bootstrap/bootstrap.actions';
 
 @Injectable()
 export class UserEffects {
-  @Effect({ dispatch: false })
-  userProfileRequest$ = this.actions$.pipe(
-    ofType<UserProfileRequestAction>(UserActionTypes.UserProfileRequest),
-    concatMap(() => {
-      return this.userService.getBasicProfile().pipe(
-        map((response: BasicProfileResponse) => {
-          if (response.status === 'error') {
-            console.error(
-              'Error getting basic profile: ' + response.status_message
-            );
-          } else {
-            this.store.dispatch(
-              new LoadBasicProfileAction(response.data.basic_profile)
-            );
-          }
-        }),
-        catchError((err) => {
-          return of(console.error(err));
-        })
-      );
-    }),
-    concatMap(() => {
-      return this.userService.getEmailProfile().pipe(
-        map((response: EmailProfileResponse) => {
-          if (response.status === 'error') {
-            console.error(
-              'Error getting email profile: ' + response.status_message
-            );
-          }
-          this.store.dispatch(
-            new LoadEmailProfileAction(response.data.email_profile)
+  userProfileRequest$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType<UserProfileRequestAction>(UserActionTypes.UserProfileRequest),
+        concatMap(() => {
+          return this.userService.getBasicProfile().pipe(
+            map((response: BasicProfileResponse) => {
+              if (response.status === 'error') {
+                console.error(
+                  'Error getting basic profile: ' + response.status_message
+                );
+              } else {
+                this.store.dispatch(
+                  new LoadBasicProfileAction(response.data.basic_profile)
+                );
+              }
+            }),
+            catchError((err) => {
+              return of(console.error(err));
+            })
           );
         }),
-        catchError((err) => {
-          return of(console.error(err));
+        concatMap(() => {
+          return this.userService.getEmailProfile().pipe(
+            map((response: EmailProfileResponse) => {
+              if (response.status === 'error') {
+                console.error(
+                  'Error getting email profile: ' + response.status_message
+                );
+              }
+              this.store.dispatch(
+                new LoadEmailProfileAction(response.data.email_profile)
+              );
+            }),
+            catchError((err) => {
+              return of(console.error(err));
+            })
+          );
+        }),
+        map(() => {
+          this.store.dispatch(new FirstRunStatusRequestAction());
         })
-      );
-    }),
-    map(() => {
-      this.store.dispatch(new FirstRunStatusRequestAction());
-    })
+      ),
+    { dispatch: false }
   );
 
-  @Effect()
-  onChange$ = fromEvent<StorageEvent>(window, 'storage').pipe(
-    // listen to our storage key
-    filter((evt) => {
-      return evt.key === 'app-user';
-    }),
-    filter((evt) => evt.newValue !== null),
-    map((evt) => {
-      let userState = JSON.parse(evt.newValue);
-      return new UpdateUserStateAction(userState);
-    })
+  onChange$ = createEffect(() =>
+    fromEvent<StorageEvent>(window, 'storage').pipe(
+      // listen to our storage key
+      filter((evt) => {
+        return evt.key === 'app-user';
+      }),
+      filter((evt) => evt.newValue !== null),
+      map((evt) => {
+        let userState = JSON.parse(evt.newValue);
+        return new UpdateUserStateAction(userState);
+      })
+    )
   );
 
   constructor(
