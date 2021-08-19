24d1af7a14db5bd40b63b9877e0d1173d15a7393
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-08-19 02:30:59 -0700

    removed logging everytime categories come back

--- src/app/core/state/settings/settings.effects.ts
+++ src/app/core/state/settings/settings.effects.ts
@@ -89,7 +89,6 @@ export class SettingsEffects {
                 this.logService.error(categoriesResponse.status_message);
               } else {
                 let categories = categoriesResponse.data.categories;
-                this.logService.log(categories);
                 this.store.dispatch(
                   new SettingsSetCategoriesAction({ categories: categories })
                 );
