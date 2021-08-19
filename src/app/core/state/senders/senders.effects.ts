101ebbce681da3fbdc700b54d3345f38ab5bed1a
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-08-18 18:12:06 -0700

    removed deprecated @Effect()

--- src/app/core/state/senders/senders.effects.ts
+++ src/app/core/state/senders/senders.effects.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ofType, Effect, Actions } from '@ngrx/effects';
+import { ofType, Actions, createEffect } from '@ngrx/effects';
 import {
   SendersActionTypes,
   SendersRequestAction,
@@ -12,6 +12,7 @@ import { SendersService } from '../../services/senders/senders.service';
 import { ISender } from './model/senders.model';
 import { of, fromEvent } from 'rxjs';
 import { NotificationService } from '../../services/notifications/notification.service';
+import { LogService } from '@app/core/services/log/log.service';
 
 @Injectable()
 export class SendersEffects {
@@ -48,60 +49,64 @@ export class SendersEffects {
     return 'XS';
   }
 
-  @Effect()
-  allSendersRequested$ = this.actions$.pipe(
-    ofType<SendersRequestAction>(SendersActionTypes.SendersRequest),
-    exhaustMap((action) => {
-      return this.sendersService.getSenders().pipe(
-        map((response) => {
-          let senders: ISender[] = response.data.senders.map((sender) => {
-            let totalSizeEstimate = this.toMB(sender.totalSizeEstimate);
-            let sizeGroup = this.getSizeGroup(totalSizeEstimate);
-            return {
-              senderId: sender.senderId,
-              fromAddress: sender.senderAddress,
-              fromName: sender.senderNames[0],
-              labelNames: [sender.senderNames[0]],
-              threadIdCount: sender.threadIdCount,
-              totalSizeEstimate: totalSizeEstimate,
-              sizeGroup: sizeGroup,
-              unsubscribeEmail: sender.unsubscribeEmail,
-              unsubscribeWeb: sender.unsubscribeWeb,
-              unsubscribed: sender.unsubscribed,
-              messageIdCount: sender.messageIdCount
-            };
-          });
-          return new AddAllSendersAction({ senders: senders });
-        }),
-        catchError((err) => {
-          this.notificationService.connectionError();
-          console.error(err);
-          return of(new SendersRequestFailureAction());
-        })
-      );
-    }),
-    catchError((err) => {
-      console.error(err);
-      return of(new SendersRequestFailureAction());
-    })
+  allSendersRequested$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType<SendersRequestAction>(SendersActionTypes.SendersRequest),
+      exhaustMap(() => {
+        return this.sendersService.getSenders().pipe(
+          map((response) => {
+            let senders: ISender[] = response.data.senders.map((sender) => {
+              let totalSizeEstimate = this.toMB(sender.totalSizeEstimate);
+              let sizeGroup = this.getSizeGroup(totalSizeEstimate);
+              return {
+                senderId: sender.senderId,
+                fromAddress: sender.senderAddress,
+                fromName: sender.senderNames[0],
+                labelNames: [sender.senderNames[0]],
+                threadIdCount: sender.threadIdCount,
+                totalSizeEstimate: totalSizeEstimate,
+                sizeGroup: sizeGroup,
+                unsubscribeEmail: sender.unsubscribeEmail,
+                unsubscribeWeb: sender.unsubscribeWeb,
+                unsubscribed: sender.unsubscribed,
+                messageIdCount: sender.messageIdCount
+              };
+            });
+            return new AddAllSendersAction({ senders: senders });
+          }),
+          catchError((err) => {
+            this.notificationService.connectionError();
+            console.error(err);
+            return of(new SendersRequestFailureAction());
+          })
+        );
+      }),
+      catchError((err) => {
+        this.logService.error(err);
+        return of(new SendersRequestFailureAction());
+      })
+    )
   );
 
-  @Effect()
-  onChange$ = fromEvent<StorageEvent>(window, 'storage').pipe(
-    // listen to our storage key
-    filter((evt) => {
-      return evt.key === 'go-app-senders';
-    }),
-    filter((evt) => evt.newValue !== null),
-    map((evt) => {
-      let suggestionsState = JSON.parse(evt.newValue);
-      return new UpdateSendersStateAction(suggestionsState);
-    })
+  // @Effect()
+  onChange$ = createEffect(() =>
+    fromEvent<StorageEvent>(window, 'storage').pipe(
+      // listen to our storage key
+      filter((evt) => {
+        return evt.key === 'zi-app-senders';
+      }),
+      filter((evt) => evt.newValue !== null),
+      map((evt) => {
+        let suggestionsState = JSON.parse(evt.newValue);
+        return new UpdateSendersStateAction(suggestionsState);
+      })
+    )
   );
 
   constructor(
     private sendersService: SendersService,
     private actions$: Actions,
-    private notificationService: NotificationService
+    private notificationService: NotificationService,
+    private logService: LogService
   ) {}
 }
