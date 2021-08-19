5eb2ca4168cad40d36b1f819c61294d59c840a1b
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-08-18 17:56:31 -0700

    removed deprecated  @Effect() decorator

--- src/app/core/state/bootstrap/bootstrap.effects.ts
+++ src/app/core/state/bootstrap/bootstrap.effects.ts
@@ -1,5 +1,5 @@
-import { Injectable, NgZone } from '@angular/core';
-import { Effect, Actions, ofType } from '@ngrx/effects';
+import { Injectable } from '@angular/core';
+import { Actions, ofType, createEffect } from '@ngrx/effects';
 import {
   BootstrapActionTypes,
   BootstrapAppAction,
@@ -13,7 +13,7 @@ import {
   UpdatePercentDownloadedAction,
   UpdateBootstrapStateAction,
   LoadAllDataRequestAction,
-  DownloadSendersRequestSuccessAction,
+  // DownloadSendersRequestSuccessAction,
   UpdateIsBootstrappedAction
 } from './bootstrap.actions';
 import { BootstrapService } from '@app/core/services//bootstrap/bootstrap.service';
@@ -25,9 +25,9 @@ import {
   exhaustMap,
   concatMap,
   filter,
-  withLatestFrom,
-  delay,
-  tap
+  // withLatestFrom,
+  delay
+  // tap
 } from 'rxjs/operators';
 import { of, fromEvent } from 'rxjs';
 import { SendersRequestAction } from '../senders/senders.actions';
@@ -42,149 +42,173 @@ export const DECIMAL = 100;
 
 @Injectable()
 export class BootstrapEffects {
-  @Effect()
-  getAllSuggestions$ = this.actions$.pipe(
-    ofType<BootstrapAppAction>(BootstrapActionTypes.BoostrapApp),
-    map((action) => {
-      return new UserProfileRequestAction();
-    })
+  getAllSenders$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType<BootstrapAppAction>(BootstrapActionTypes.BoostrapApp),
+      map((action) => {
+        return new UserProfileRequestAction();
+      })
+    )
   );
 
-  @Effect({ dispatch: false })
-  firstRunStatusRequested$ = this.actions$.pipe(
-    ofType<FirstRunStatusRequestAction>(
-      BootstrapActionTypes.FirstRunStatusRequest
-    ),
-    exhaustMap(() => {
-      return this.bootstrapService.getFirstRunStatus().pipe(
-        map((response) => {
-          if (response.status === 'error') {
-            this.store.dispatch(new FirstRunStatusRequestFailureAction());
-          }
-          this.store.dispatch(
-            new UpdateFirstRunAction({ firstRun: response.data.firstRun })
+  firstRunStatusRequested$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType<FirstRunStatusRequestAction>(
+          BootstrapActionTypes.FirstRunStatusRequest
+        ),
+        exhaustMap(() => {
+          return this.bootstrapService.getFirstRunStatus().pipe(
+            map((response) => {
+              if (response.status === 'error') {
+                this.store.dispatch(new FirstRunStatusRequestFailureAction());
+              }
+              this.store.dispatch(
+                new UpdateFirstRunAction({ firstRun: response.data.firstRun })
+              );
+              if (response.data.firstRun === true) {
+                this.store.dispatch(
+                  new UpdateDownloadingStatusAction({ downloadingStatus: true })
+                );
+                this.router.navigate([
+                  this.bootstrapService.downloadingSendersUrl
+                ]);
+              } else {
+                this.store.dispatch(
+                  new DownloadSendersRequestAction({ firstRunStatus: false })
+                );
+                this.store.dispatch(
+                  new UpdateIsBootstrappedAction({ isBootstrapped: true })
+                );
+                this.router.navigate([
+                  this.bootstrapService.sendersDownloadedUrl
+                ]);
+              }
+            }),
+            catchError((err) => {
+              console.error(err);
+              return of(new FirstRunStatusRequestFailureAction());
+            })
           );
-          if (response.data.firstRun === true) {
-            this.store.dispatch(
-              new UpdateDownloadingStatusAction({ downloadingStatus: true })
-            );
-            this.router.navigate([this.bootstrapService.downloadingSendersUrl]);
-          } else {
-            this.store.dispatch(
-              new DownloadSendersRequestAction({ firstRunStatus: false })
-            );
-            this.store.dispatch(
-              new UpdateIsBootstrappedAction({ isBootstrapped: true })
-            );
-            this.router.navigate([this.bootstrapService.sendersDownloadedUrl]);
-          }
-        }),
-        catchError((err) => {
-          console.error(err);
-          return of(new FirstRunStatusRequestFailureAction());
         })
-      );
-    })
+      ),
+    { dispatch: false }
   );
 
-  @Effect()
-  loadSuggestionsRequest$ = this.actions$.pipe(
-    ofType<DownloadSendersRequestAction>(
-      BootstrapActionTypes.DownloadSendersRequest
-    ),
-    exhaustMap((action) => {
-      return this.bootstrapService.getLoadSuggestions().pipe(
-        map((response) => {
-          if (response.status === 'error') {
-            return new DownloadSendersRequestFailureAction();
-          } else {
-            if (action.payload.firstRunStatus) {
-              return new DownloadingStatusRequestAction();
+  loadSendersRequest$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType<DownloadSendersRequestAction>(
+        BootstrapActionTypes.DownloadSendersRequest
+      ),
+      exhaustMap((action) => {
+        return this.bootstrapService.getLoadSuggestions().pipe(
+          map((response) => {
+            if (response.status === 'error') {
+              return new DownloadSendersRequestFailureAction();
             } else {
-              return new LoadAllDataRequestAction();
+              if (action.payload.firstRunStatus) {
+                return new DownloadingStatusRequestAction();
+              } else {
+                return new LoadAllDataRequestAction();
+              }
             }
-          }
-        }),
-        catchError((err) => {
-          console.error(err);
-          return of(new DownloadSendersRequestFailureAction());
-        })
-      );
-    }),
-    catchError((err) => {
-      console.error(err);
-      return of(new DownloadSendersRequestFailureAction());
-    })
+          }),
+          catchError((err) => {
+            console.error(err);
+            return of(new DownloadSendersRequestFailureAction());
+          })
+        );
+      }),
+      catchError((err) => {
+        console.error(err);
+        return of(new DownloadSendersRequestFailureAction());
+      })
+    )
   );
 
-  @Effect({ dispatch: false })
-  getLoadingStatus$ = this.actions$.pipe(
-    ofType<DownloadingStatusRequestAction>(
-      BootstrapActionTypes.DownloadingStatusRequest
-    ),
-    delay(1000),
-    concatMap((action) => {
-      return this.bootstrapService.getLoadingStatus().pipe(
-        map((response) => {
-          if (response.status === 'error') {
-            this.store.dispatch(new DownloadingStatusRequestFailureAction());
-          }
-          if (response.data.loadingStatus === true) {
-            let percentLoaded = response.data.percentLoaded;
-            this.store.dispatch(
-              new UpdatePercentDownloadedAction({
-                percentLoaded: percentLoaded
-              })
-            );
-            this.store.dispatch(new DownloadingStatusRequestAction());
-          } else {
-            this.store.dispatch(
-              new UpdateDownloadingStatusAction({ downloadingStatus: false })
-            );
-            this.store.dispatch(
-              new UpdateIsBootstrappedAction({ isBootstrapped: true })
-            );
-            this.router.navigate([this.bootstrapService.sendersDownloadedUrl]);
-            this.store.dispatch(new LoadAllDataRequestAction());
-          }
-        }),
-        catchError((err) => {
-          console.error(err);
-          return of(
-            this.store.dispatch(new DownloadingStatusRequestFailureAction())
+  getLoadingStatus$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType<DownloadingStatusRequestAction>(
+          BootstrapActionTypes.DownloadingStatusRequest
+        ),
+        delay(1000),
+        concatMap((action) => {
+          return this.bootstrapService.getLoadingStatus().pipe(
+            map((response) => {
+              if (response.status === 'error') {
+                this.store.dispatch(
+                  new DownloadingStatusRequestFailureAction()
+                );
+              }
+              if (response.data.loadingStatus === true) {
+                let percentLoaded = response.data.percentLoaded;
+                this.store.dispatch(
+                  new UpdatePercentDownloadedAction({
+                    percentLoaded: percentLoaded
+                  })
+                );
+                this.store.dispatch(new DownloadingStatusRequestAction());
+              } else {
+                this.store.dispatch(
+                  new UpdateDownloadingStatusAction({
+                    downloadingStatus: false
+                  })
+                );
+                this.store.dispatch(
+                  new UpdateIsBootstrappedAction({ isBootstrapped: true })
+                );
+                this.router.navigate([
+                  this.bootstrapService.sendersDownloadedUrl
+                ]);
+                this.store.dispatch(new LoadAllDataRequestAction());
+              }
+            }),
+            catchError((err) => {
+              console.error(err);
+              return of(
+                this.store.dispatch(new DownloadingStatusRequestFailureAction())
+              );
+            })
           );
         })
-      );
-    })
+      ),
+    { dispatch: false }
   );
 
-  @Effect({ dispatch: false })
-  loadAllData$ = this.actions$.pipe(
-    ofType<LoadAllDataRequestAction>(BootstrapActionTypes.LoadAllDataRequest),
-    map(() => {
-      this.store.dispatch(new SendersRequestAction());
-      this.store.dispatch(new SettingsGetCategoriesRequestAction());
-    })
+  loadAllData$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType<LoadAllDataRequestAction>(
+          BootstrapActionTypes.LoadAllDataRequest
+        ),
+        map(() => {
+          this.store.dispatch(new SendersRequestAction());
+          this.store.dispatch(new SettingsGetCategoriesRequestAction());
+        })
+      ),
+    { dispatch: false }
   );
 
-  @Effect()
-  onChange$ = fromEvent<StorageEvent>(window, 'storage').pipe(
-    // listen to our storage key
-    filter((evt) => {
-      return evt.key === 'go-app-bootstrap';
-    }),
-    filter((evt) => evt.newValue !== null),
-    map((evt) => {
-      let BootstrapState = JSON.parse(evt.newValue);
-      return new UpdateBootstrapStateAction(BootstrapState);
-    })
+  onChange$ = createEffect(() =>
+    fromEvent<StorageEvent>(window, 'storage').pipe(
+      // listen to our storage key
+      filter((evt) => {
+        return evt.key === 'go-app-bootstrap';
+      }),
+      filter((evt) => evt.newValue !== null),
+      map((evt) => {
+        let BootstrapState = JSON.parse(evt.newValue);
+        return new UpdateBootstrapStateAction(BootstrapState);
+      })
+    )
   );
 
   constructor(
     private actions$: Actions,
     private router: Router,
     private bootstrapService: BootstrapService,
-    private ngZone: NgZone,
+    // private ngZone: NgZone,
     private store: Store<AppState>
   ) {}
 }
